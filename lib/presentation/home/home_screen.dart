83fcaf47d47213d1870af0cb8c29e2a25c1d625f
Amey Reghu <amey.reghu@centelon.com>
2024-02-15 12:17:01 +0530

    mc

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:rive/rive.dart';
+import 'package:tpr_control_interface_linux/presentation/product_explanation/video_showcase_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,36 +12,45 @@ class HomeScreen extends StatelessWidget {
     var isProcessing = ValueNotifier(false);
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-                // color: Colors.red,
-                padding: const EdgeInsets.all(2),
-                height: size.height * 0.67,
-                width: double.infinity,
-                child: const RiveAnimation.asset(
-                  'assets/rive/sq_eyeL.riv',
-                  fit: BoxFit.contain,
-                )),
-            ValueListenableBuilder(
-              valueListenable: isProcessing,
-              builder: (context, value, child) {
-                if (value) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 60),
-                    child: LottieBuilder.asset(
-                      'assets/lottieanimations/pre.json',
-                      height: size.height * 0.2,
-                      // fit: BoxFit.fitHeight,
-                    ),
-                  );
-                } else {
-                  return const SizedBox();
-                }
-              },
-            )
-          ],
+      body: GestureDetector(
+        onLongPress: () {
+          Future.delayed(const Duration(seconds: 3), () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const VideoShowCaseScreen(),
+            ));
+          });
+        },
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                  // color: Colors.red,
+                  padding: const EdgeInsets.all(2),
+                  height: size.height * 0.67,
+                  width: double.infinity,
+                  child: const RiveAnimation.asset(
+                    'assets/rive/sq_eyeL.riv',
+                    fit: BoxFit.contain,
+                  )),
+              ValueListenableBuilder(
+                valueListenable: isProcessing,
+                builder: (context, value, child) {
+                  if (value) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 60),
+                      child: LottieBuilder.asset(
+                        'assets/lottieanimations/pre.json',
+                        height: size.height * 0.2,
+                        // fit: BoxFit.fitHeight,
+                      ),
+                    );
+                  } else {
+                    return const SizedBox();
+                  }
+                },
+              )
+            ],
+          ),
         ),
       ),
     );

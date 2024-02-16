0110d88cbe3825ad31906e32d7eaee62e83e11ba
Amey Reghu <amey.reghu@centelon.com>
2024-02-16 12:31:39 +0530

    fullscreen and no title bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,25 @@ import 'package:flutter/material.dart';
 
 import 'package:tpr_control_interface_linux/presentation/control_interface_app.dart';
 import 'package:fvp/fvp.dart' as fvp;
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
+
+  WindowOptions wo = WindowOptions(
+    alwaysOnTop: true,
+    fullScreen: true,
+    titleBarStyle: TitleBarStyle.hidden,
+
+  );
+
+    windowManager.waitUntilReadyToShow(wo, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   initVideoPlayerMediaKit();
   runApp(const ControlInterfaceApp());
 }

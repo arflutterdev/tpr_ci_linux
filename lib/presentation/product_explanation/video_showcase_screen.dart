03caab78cd905dda989aeaeaa37ec80922ec37a6
Amey Reghu <amey.reghu@centelon.com>
2024-03-11 15:11:40 +0530

    disabled fvp

--- lib/presentation/product_explanation/video_showcase_screen.dart
+++ lib/presentation/product_explanation/video_showcase_screen.dart
@@ -1,45 +1,45 @@
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-import 'package:fvp/fvp.dart';
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+// import 'package:fvp/fvp.dart';
 
-class VideoShowCaseScreen extends StatefulWidget {
-  const VideoShowCaseScreen({super.key});
+// class VideoShowCaseScreen extends StatefulWidget {
+//   const VideoShowCaseScreen({super.key});
 
-  @override
-  State<VideoShowCaseScreen> createState() => _VideoShowCaseScreenState();
-}
+//   @override
+//   State<VideoShowCaseScreen> createState() => _VideoShowCaseScreenState();
+// }
 
-class _VideoShowCaseScreenState extends State<VideoShowCaseScreen> {
-  late VideoPlayerController _controller;
+// class _VideoShowCaseScreenState extends State<VideoShowCaseScreen> {
+//   late VideoPlayerController _controller;
 
-  @override
-  void initState() {
-    _controller = VideoPlayerController.asset(
-        'assets/videos/foresttamil.mp4')
-      ..initialize().then((_) {
-        setState(() {});
-        _controller.play();
-      });
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     _controller = VideoPlayerController.asset(
+//         'assets/videos/foresttamil.mp4')
+//       ..initialize().then((_) {
+//         setState(() {});
+//         _controller.play();
+//       });
+//     super.initState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-      ),
-      body: Center(
-        child: VideoPlayer(_controller),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       extendBodyBehindAppBar: true,
+//       appBar: AppBar(
+//         backgroundColor: Colors.transparent,
+//       ),
+//       body: Center(
+//         child: VideoPlayer(_controller),
+//       ),
+//     );
+//   }
 
-  @override
-  void dispose() {
-    _controller.pause();
-    _controller.dispose();
-    super.dispose();
-  }
-}
+//   @override
+//   void dispose() {
+//     _controller.pause();
+//     _controller.dispose();
+//     super.dispose();
+//   }
+// }

fd9778680bc81a9846bc555f6e6776e1b6fbad2a
Amey Reghu <amey.reghu@centelon.com>
2024-03-12 10:15:39 +0530

    renabled full screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,19 @@ import 'package:window_manager/window_manager.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //Must add this line.
-  // await windowManager.ensureInitialized();
+  await windowManager.ensureInitialized();
 
-  // WindowOptions wo = WindowOptions(
-  //   alwaysOnTop: true,
-  //   fullScreen: true,
-  //   titleBarStyle: TitleBarStyle.hidden,
+  WindowOptions wo = WindowOptions(
+    alwaysOnTop: true,
+    fullScreen: true,
+    titleBarStyle: TitleBarStyle.hidden,
 
-  // );
+  );
 
-  //   windowManager.waitUntilReadyToShow(wo, () async {
-  //   await windowManager.show();
-  //   await windowManager.focus();
-  // });
+    windowManager.waitUntilReadyToShow(wo, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
 
   initVideoPlayerMediaKit();
   runApp(const ControlInterfaceApp());
